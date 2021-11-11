db149f24ef307e474995febee905848c49771ef2
MalcolmFFS <57695258+MalcolmFFS@users.noreply.github.com>
2021-11-11 06:21:07 -0700

    yikes day 11 of 2020

--- 2020/advent_11.py
+++ 2020/advent_11.py
@@ -74,6 +74,179 @@ def part_one(the_input):
     return new_input
 
 
+def part_two(the_input):
+    def find_first(the_adjacent):
+        adj_row, adj_col = the_adjacent
+        char = the_input[adj_row][adj_col]
+        if char == '#':
+            return '#'
+        elif char == 'L':
+            return 'L'
+        else:
+            return '.'
+
+    new_input = []
+    for my_row in the_input:
+        new_input.append(my_row[:])
+    for row_index, my_row in enumerate(the_input):
+        for col_index, my_column in enumerate(my_row):
+            my_row_index = row_index
+            my_col_index = col_index
+            # print(row_index, col_index)
+            adjacents = []
+            condition_up = my_row_index != 0
+            condition_left = my_col_index != 0
+            condition_down = my_row_index != len(the_input) - 1
+            condition_right = my_col_index != len(my_row) - 1
+
+            my_char = the_input[row_index][col_index]
+            if my_char == 'L' or my_char == '#':
+                i = 1
+                while condition_up:
+                    try:
+                        my_row_index = row_index - i
+                        if my_row_index >= 0:
+                            first = find_first((my_row_index, col_index))
+                        else:
+                            break
+                    except IndexError:
+                        break
+                    if first == '#' or first == 'L':
+                        adjacents.append(first)
+                        # print("Broke condition_up")
+                        break
+                    i += 1
+
+                i = 1
+                while condition_up and condition_right:
+                    try:
+                        my_row_index = row_index - i
+                        my_col_index = col_index + i
+                        if my_row_index >= 0 and my_col_index >= 0:
+                            first = find_first((my_row_index, my_col_index))
+                        else:
+                            break
+                    except IndexError:
+                        break
+                    if first == '#' or first == 'L':
+                        adjacents.append(first)
+                        # print("Broke condition_up and condition_right")
+                        break
+                    i += 1
+
+                i = 1
+                while condition_right:
+                    try:
+                        my_col_index = col_index + i
+                        if my_col_index >= 0:
+                            first = find_first((row_index, my_col_index))
+                        else:
+                            break
+                    except IndexError:
+                        break
+                    if first == '#' or first == 'L':
+                        adjacents.append(first)
+                        # print("Broke condition_right")
+                        break
+                    i += 1
+
+                i = 1
+                while condition_right and condition_down:
+                    # print("Checking right and down")
+                    # print(condition_right, condition_down)
+                    # print(row_index, len(the_input) - 1)
+                    # print(col_index, len(my_row) - 1)
+                    try:
+                        my_row_index = row_index + i
+                        my_col_index = col_index + i
+                        if my_row_index >= 0 and my_col_index >= 0:
+                            first = find_first((my_row_index, my_col_index))
+                        else:
+                            break
+                    except IndexError:
+                        break
+                    if first == '#' or first == 'L':
+                        adjacents.append(first)
+                        # print("Broke condition_right and condition_down")
+                        break
+                    i += 1
+
+                i = 1
+                while condition_down:
+                    try:
+                        my_row_index = row_index + i
+                        if my_row_index >= 0:
+                            first = find_first((my_row_index, col_index))
+                        else:
+                            break
+                    except IndexError:
+                        break
+                    if first == '#' or first == 'L':
+                        adjacents.append(first)
+                        # print("Broke condition_down")
+                        break
+                    i += 1
+
+                i = 1
+                while condition_down and condition_left:
+                    try:
+                        my_row_index = row_index + i
+                        my_col_index = col_index - i
+                        if my_row_index >= 0 and my_col_index >= 0:
+                            first = find_first((my_row_index, my_col_index))
+                        else:
+                            break
+                    except IndexError:
+                        break
+                    if first == '#' or first == 'L':
+                        adjacents.append(first)
+                        # print("Broke condition_down and condition_left")
+                        break
+                    i += 1
+
+                i = 1
+                while condition_left:
+                    try:
+                        my_col_index = col_index - i
+                        if my_col_index >= 0:
+                            first = find_first((row_index, my_col_index))
+                        else:
+                            break
+                    except IndexError:
+                        break
+                    if first == '#' or first == 'L':
+                        adjacents.append(first)
+                        # print("Broke condition_left")
+                        break
+                    i += 1
+
+                i = 1
+                while condition_left and condition_up:
+                    try:
+                        my_row_index = row_index - i
+                        my_col_index = col_index - i
+                        if my_row_index >= 0 and my_col_index >= 0:
+                            first = find_first((my_row_index, my_col_index))
+                        else:
+                            break
+                    except IndexError:
+                        break
+                    if first == '#' or first == 'L':
+                        adjacents.append(first)
+                        # print("Broke condition_left and condition_up")
+                        break
+                    i += 1
+                # print(adjacents)
+                # print(f"({row_index},{col_index})")
+
+                if adjacents.count('#') == 0 and the_input[row_index][col_index] == 'L':
+                    new_input[row_index][col_index] = '#'
+                elif adjacents.count('#') >= 5 and the_input[row_index][col_index] == '#':
+                    new_input[row_index][col_index] = 'L'
+
+    return new_input
+
+
 if __name__ == "__main__":
     my_input = the_setup()
     shuffle_count = 0
@@ -89,8 +262,27 @@ if __name__ == "__main__":
 
     occupied_count = 0
     for row in my_input:
-        for column in row:
-            if column == '#':
-                occupied_count += 1
+        occupied_count += row.count('#')
+
+    print(occupied_count)
+
+    my_input = the_setup()
+    shuffle_count = 0
+    while True:
+        test_shuffle = part_two(my_input[:])
+        # for row in test_shuffle:
+        #     print(row, row.count('#'))
+        # print()
+        if test_shuffle == my_input:
+            print("They match!")
+            print(f"Shuffles: {shuffle_count}")
+            break
+        else:
+            my_input = test_shuffle
+            shuffle_count += 1
+
+    occupied_count = 0
+    for row in my_input:
+        occupied_count += row.count('#')
 
     print(occupied_count)
