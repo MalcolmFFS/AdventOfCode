c1ddfa21b87bd0abda5ff845513c05cd53d63436
MalcolmFFS <57695258+MalcolmFFS@users.noreply.github.com>
2021-11-16 22:35:14 -0700

    Functional but slow on part2

--- 2020/advent_15.py
+++ 2020/advent_15.py
@@ -1,36 +1,36 @@
 #!/usr/bin/env python3
 
 
-def part_one(the_input):
+def part_one(the_input, last):
     occurrences = dict()
-    full_list = list()
+    last_num = None
     index = 0
-    while index < 2020:
+    while index < last:
         try:
             i = the_input[index]
-            full_list.append(i)
+            last_num = i
             if i not in occurrences:
                 occurrences[i] = list()
             occurrences[i].append(index)
         except IndexError:
-            i = full_list[index - 1]
+            i = last_num
             if len(occurrences[i]) == 1:
                 if 0 not in occurrences:
                     occurrences[0] = list()
                 occurrences[0].append(index)
-                full_list.append(0)
+                last_num = 0
             else:
                 diff = occurrences[i][-1] - occurrences[i][-2]
                 if diff not in occurrences:
                     occurrences[diff] = list()
                 occurrences[diff].append(index)
-                full_list.append(diff)
+                last_num = diff
         index += 1
 
-    return full_list[-1]
+    return last_num
 
 
 if __name__ == "__main__":
     my_input = [15, 12, 0, 14, 3, 1]
-    # my_input = [2,1,3]
-    print(part_one(my_input[:]))
+    print(f"Part one output: {part_one(my_input[:], 2020)}.")
+    print(f"Part two output: {part_one(my_input[:], 30000000)}.")
--- 2020/test_advent_15.py
+++ 2020/test_advent_15.py
@@ -7,13 +7,22 @@ from advent_15 import part_one
 
 class TestAdvent15(unittest.TestCase):
     def test_part_one(self):
-        self.assertEqual(part_one([0, 3, 6]), 436)
-        self.assertEqual(part_one([1, 3, 2]), 1)
-        self.assertEqual(part_one([2, 1, 3]), 10)
-        self.assertEqual(part_one([1, 2, 3]), 27)
-        self.assertEqual(part_one([2, 3, 1]), 78)
-        self.assertEqual(part_one([3, 2, 1]), 438)
-        self.assertEqual(part_one([3, 1, 2]), 1836)
+        self.assertEqual(part_one([0, 3, 6], 2020), 436)
+        self.assertEqual(part_one([1, 3, 2], 2020), 1)
+        self.assertEqual(part_one([2, 1, 3], 2020), 10)
+        self.assertEqual(part_one([1, 2, 3], 2020), 27)
+        self.assertEqual(part_one([2, 3, 1], 2020), 78)
+        self.assertEqual(part_one([3, 2, 1], 2020), 438)
+        self.assertEqual(part_one([3, 1, 2], 2020), 1836)
+
+    def test_part_two(self):
+        self.assertEqual(part_one([0, 3, 6], 30000000), 175594)
+        # self.assertEqual(part_one([1, 3, 2], 30000000), 2578)
+        # self.assertEqual(part_one([2, 1, 3], 30000000), 3544142)
+        # self.assertEqual(part_one([1, 2, 3], 30000000), 261214)
+        # self.assertEqual(part_one([2, 3, 1], 30000000), 6895259)
+        # self.assertEqual(part_one([3, 2, 1], 30000000), 18)
+        # self.assertEqual(part_one([3, 1, 2], 30000000), 362)
 
 
 if __name__ == "__main__":
