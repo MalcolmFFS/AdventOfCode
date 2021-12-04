a2dbc936a7ca007ddde6ca584d28becabdac6580
MalcolmFFS <57695258+MalcolmFFS@users.noreply.github.com>
2021-12-04 09:12:00 -0700

    Cleaned up day 4 some... not sure what readable way I could use to lower nested for loops

--- 2021/advent_04.py
+++ 2021/advent_04.py
@@ -9,76 +9,72 @@ def the_setup():
     with open('input_04.txt') as f_object:
         tmp = f_object.read().split('\n\n')
 
-    tmp_numbers_called = tmp[0].split(',')
-    the_numbers_called = [int(n) for n in tmp_numbers_called]
-    the_boards = dict()
-    for board_index, board in enumerate(tmp[1:]):
-        tmp_board = board.split('\n')
-        the_boards[board_index] = list()
-        for row in tmp_board:
-            the_boards[board_index].append([[int(column), 0] for column in row.split()])
+    nums = [int(n) for n in tmp[0].split(',')]
+    boards = [[[
+        [int(col), 0] for col in row.split()]
+        for row in board.split('\n')]
+        for board in tmp[1:]
+    ]
 
-    return the_numbers_called, the_boards
+    return nums, boards
 
 
-def part_one(the_numbers_called, the_boards):
-    def run_game(thy_numbers_called, thy_boards):
-        for n in thy_numbers_called:
-            for board in thy_boards.values():
-                for row in board:
-                    for col_index, column in enumerate(row):
-                        if column[0] == n:
-                            column[1] = 1
-                            if sum([col[1] for col in row]) == 5:
-                                return board, n
-                            if sum([row[col_index][1] for row in board]) == 5:
-                                return board, n
-                        else:
-                            continue
-
-    winning_board, last_num = run_game(the_numbers_called, the_boards)
+def calculate_board_score(board, num):
     unmarked_nums = []
-    for my_row in winning_board:
+    for my_row in board:
         for my_col in my_row:
             if my_col[1] == 0:
                 unmarked_nums.append(my_col[0])
 
-    return sum(unmarked_nums) * last_num
+    return sum(unmarked_nums) * num
 
 
-def part_two(the_numbers_called, the_boards):
-    def run_game(thy_numbers_called, thy_boards):
-        for n in thy_numbers_called:
-            tmp_boards = thy_boards.copy()
-            for board_index, board in tmp_boards.items():
-                for row in board:
-                    for col_index, column in enumerate(row):
-                        if column[0] == n:
-                            column[1] = 1
-                            if sum([col[1] for col in row]) == 5:
-                                if len(tmp_boards) > 1:
-                                    del thy_boards[board_index]
-                                    continue
-                                else:
-                                    return board, n
+def part_one(numbers_called, boards):
+    for n in numbers_called:
+        for board in boards:
+            for row in board:
+                for col_index, column in enumerate(row):
+                    if column[0] == n:
+                        column[1] = 1
+                        cond1 = sum([col[1] for col in row]) == 5
+                        cond2 = sum([row[col_index][1] for row in board]) == 5
 
-                            if sum([row[col_index][1] for row in board]) == 5:
-                                if len(tmp_boards) > 1:
-                                    del thy_boards[board_index]
-                                    continue
-                                else:
-                                    return board, n
-                        else:
-                            continue
+                        if cond1 or cond2:
+                            return calculate_board_score(board, n)
+                    else:
+                        continue
 
-    winning_board, last_num = run_game(the_numbers_called, the_boards)
-    unmarked_nums = []
-    for my_row in winning_board:
-        for my_col in my_row:
-            if my_col[1] == 0:
-                unmarked_nums.append(my_col[0])
 
-    return sum(unmarked_nums) * last_num
+def part_two(numbers_called, boards):
+    for n in numbers_called:
+        tmp_boards = boards[:]
+
+        for board in tmp_boards:
+            for row in board:
+                for col_index, column in enumerate(row):
+
+                    if column[0] == n:
+                        column[1] = 1
+                        cond1 = sum([col[1] for col in row]) == 5
+                        cond2 = sum([row[col_index][1] for row in board]) == 5
+
+                        if cond1 or cond2:
+                            if len(tmp_boards) > 1:
+                                boards.remove(board)
+                                continue
+                            else:
+                                return calculate_board_score(board, n)
+                    else:
+                        continue
+
+
+def main():
+    # To run against sample input
+    # my_input = [i for i in sample_input.strip().split('\n')]
+
+    numbers_called, boards = the_setup()
+    print(f"The final score with the best board (part one) is: {part_one(numbers_called, boards)}.")
+    print(f"The final score with the worst board (part two) is: {part_two(numbers_called, boards)}.")
 
 
 sample_input = r"""
@@ -87,9 +83,4 @@ Sample input won't work for this setup, would need to hijack the_setup() to use 
 
 
 if __name__ == "__main__":
-    # To run against sample input
-    # my_input = [i for i in sample_input.strip().split('\n')]
-
-    numbers_called, boards = the_setup()
-    print(f"The final score with the best board (part one) is: {part_one(numbers_called, boards)}.")
-    print(f"The final score with the worst board (part two) is: {part_two(numbers_called, boards)}.")
+    main()
