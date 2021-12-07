7100ef981d4f737d7e9e4d3c3d558b2f717f7965
MalcolmFFS <57695258+MalcolmFFS@users.noreply.github.com>
2021-12-07 11:01:38 -0700

    timer update and alt

--- wsl_execution_times.sh
+++ wsl_execution_times.sh
@@ -3,8 +3,8 @@
 for file in *.py; do
   file_mod=$(echo "$file" | cut -d. -f1);
   echo "$file";
-  echo "part_one():";
+  echo "part_one():"; sleep 3;
   { time python3 -c "from $file_mod import *; the_input = the_setup(); print(part_one(the_input))"; } 2>&1 | grep real;
-  echo "part_two()";
+  echo "part_two()"; sleep 3;
   { time python3 -c "from $file_mod import *; the_input = the_setup(); print(part_two(the_input))"; } 2>&1 | grep real;
 done
---
+++ wsl_execution_times_hyperfine.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+for file in *.py; do
+  file_mod=$(echo "$file" | cut -d. -f1);
+  echo "$file";
+  echo "part_one():"; sleep 3;
+  hyperfine -i --warmup 5 "python3 -c 'from $file_mod import *; the_input = the_setup(); print(part_one(the_input))'";
+  echo "part_two()"; sleep 3;
+  hyperfine -i --warmup 5 "python3 -c 'from $file_mod import *; the_input = the_setup(); print(part_two(the_input))'";
+done
