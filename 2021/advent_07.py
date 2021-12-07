d7a5bff7fa595931b1bfbc59b25eecaa748c44dc
MalcolmFFS <57695258+MalcolmFFS@users.noreply.github.com>
2021-12-06 22:58:25 -0700

    found the right maths

--- 2021/advent_07.py
+++ 2021/advent_07.py
@@ -7,38 +7,34 @@
 def the_setup():
     with open('input_07.txt') as f_object:
         for line in f_object:
-            the_input =[int(i) for i in line.strip().split(',')]
+            the_input = [int(i) for i in line.strip().split(',')]
 
     return the_input
 
 
-def part_one(the_input):
-    diffs = dict()
-    for i in range(min(the_input), max(the_input) + 1):
-        diff = 0
-        for crab in the_input:
-            diff += abs(crab - i)
-        diffs[i] = diff
+def calculate_fuel(crabs, position, base_increment=0):
+    diff = 0
+    for crab in crabs:
+        increment = 1
+        for _ in range(abs(crab - position)):
+            diff += increment
+            increment += base_increment
+    return diff
 
-    optimal_point = min(diffs, key=diffs.get)
-    return diffs[optimal_point]
+
+def part_one(the_input):
+    the_input.sort()
+    crab_median = the_input[int(len(the_input) / 2)]
+    fuel = calculate_fuel(the_input, crab_median)
+
+    return fuel
 
 
 def part_two(the_input):
-    diffs = dict()
-    for i in range(min(the_input), max(the_input) + 1):
-        diff = 0
-        for crab in the_input:
-            distance = abs(crab - i)
-            counter = 0
-            for j in range(distance):
-                counter += 1
-                diff += counter
-        print(i)
-        diffs[i] = diff
+    input_avg = int(sum(the_input) / len(the_input))
+    fuel = calculate_fuel(the_input, input_avg, 1)
 
-    optimal_point = min(diffs, key=diffs.get)
-    return diffs[optimal_point]
+    return fuel
 
 
 def main():
@@ -50,8 +46,8 @@ def main():
     # my_input = [int(i) for i in sample_input.strip().split(',')]
 
     my_input = the_setup()
-    print(f"The  (part one) is: {part_one(my_input[:])}.")
-    print(f"The  (part two) is: {part_two(my_input[:])}.")
+    print(f"The fuel consumed givin linear fuel cost (part one) is: {part_one(my_input[:])}.")
+    print(f"The fuel consumed given sequential fuel cost (part two) is: {part_two(my_input[:])}.")
 
 
 if __name__ == "__main__":
