7a12f2dae3044c224e66e70d59bede98fae64143
MalcolmFFS <57695258+MalcolmFFS@users.noreply.github.com>
2021-12-06 23:44:29 -0700

    Added bash script used to verify runtimes of all scripts in WSL, assuming scripts are in CWD.

---
+++ wsl_execution_times.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+for file in *.py; do
+  file_mod=$(echo "$file" | cut -d. -f1);
+  echo "$file";
+  echo "part_one():";
+  { time python3 -c "from $file_mod import *; the_input = the_setup(); print(part_one(the_input))"; } 2>&1 | grep real;
+  echo "part_two()";
+  { time python3 -c "from $file_mod import *; the_input = the_setup(); print(part_two(the_input))"; } 2>&1 | grep real;
+done
