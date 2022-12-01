7d82d50da6f47eec982c1cc1b5e34dc0bce6226e
MalcolmFFS <57695258+MalcolmFFS@users.noreply.github.com>
2022-11-30 22:32:03 -0700

    Prepping for 2022... How far until I fail this year?

--- 2022/advent_01.py
+++ 2022/advent_01.py
@@ -4,37 +4,59 @@
 #
 
 
-def the_setup() -> list:
-    the_input = list()
+def the_setup() -> dict:
+    # the_input = list()
     with open('input_01.txt') as f_object:
-        for line in f_object:
-            the_input.append(line.strip())
+        the_input = f_object.read()
 
-        # tmp = f_object.read().split('\n\n')
-        # a = tmp[0]
-        # the_input = [int(i) for i in tmp]
+    food_dict = dict()
+    for elf, elf_food in enumerate(the_input.split('\n\n')):
+        food_dict[elf] = [int(food) for food in elf_food.split('\n')]
 
-    return the_input
+    return food_dict
 
 
 def part_one(the_input):
-    pass
+    return max(sum(elf_food) for elf_food in the_input.values())
 
 
 def part_two(the_input):
-    pass
+    highest = list()
+
+    all_elves_max = [sum(elf_food) for elf_food in the_input.values()]
+
+    highest.append(max(all_elves_max))
+    all_elves_max.remove(max(all_elves_max))
+    highest.append(max(all_elves_max))
+    all_elves_max.remove(max(all_elves_max))
+    highest.append(max(all_elves_max))
+    all_elves_max.remove(max(all_elves_max))
+
+    return sum(highest)
 
 
 def main():
-    sample_input = """
-    """
+    #     sample_input = """1000
+    # 2000
+    # 3000
+    #
+    # 4000
+    #
+    # 5000
+    # 6000
+    #
+    # 7000
+    # 8000
+    # 9000
+    #
+    # 10000"""
 
     # To run against sample input
     # my_input = [i for i in sample_input.strip().split('\n')]
 
     my_input = the_setup()
-    print(f"The  (part one) is: {part_one(my_input)}.")
-    print(f"The  (part two) is: {part_two(my_input)}.")
+    print(f"The most calories carried by a single elf (part one) is: {part_one(my_input)}.")
+    print(f"The top 3 elves together total calories (part two) is: {part_two(my_input)}.")
 
 
 if __name__ == "__main__":
